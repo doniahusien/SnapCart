2df73bbb8e9246e863b920a11df72f3432c0e78a
doniahusien <doniahusien@gmail.com>
2025-09-07 17:19:14 +0300

    Update ProductCard and ProductsView with latest changes

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { onMounted } from 'vue';
+import ProductCard from '../components/ProductCard.vue'
+import { useProductsStore } from "../stores/products";
+const productsStore = useProductsStore();
+onMounted(async ()=> {
+    await productsStore.fetchProducts();
+})
 </script>
 
 <template>
   
+  <div class="flex flex-row flex-wrap justify-center gap-10 p-10">
+  <ProductCard v-for="item in productsStore.products" :key="item" :product="item"/>
+  </div>
  
 </template>
 
