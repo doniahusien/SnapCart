79b79a54827f7d3d1ba701ddcc113b4cedfcada8
doniahusien <doniahusien@gmail.com>
2025-09-07 23:22:07 +0300

    Add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,10 @@
-<script setup>
-</script>
-
 <template>
-  
- 
+  <div class="p-10 flex flex-col justify-center items-center text-center min-h-screen">
+    <h1 class="text-3xl font-bold mb-4">About Us</h1>
+    <p class="text-gray-600 max-w-2xl mx-auto">
+      Welcome to our store! We provide high-quality products at affordable prices.  
+      Our mission is to make shopping easy, enjoyable, and accessible for everyone.
+    </p>
+  </div>
 </template>
 
-<style scoped>
-
-
-</style>
