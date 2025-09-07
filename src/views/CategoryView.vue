7d3678d00007a2d72bff1940bd58b4c7598c06c7
doniahusien <doniahusien@gmail.com>
2025-09-07 15:32:35 +0300

    Add CategoryView page

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hi hello</h1>
+    {{ cat }}
+</template>
+<script setup>
+import { useProductsStore } from '../stores/products';
+import { useRoute } from 'vue-router';
+const cat = useRoute().params.cat;
+</script>
